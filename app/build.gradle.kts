8be8bcbfc58b2c9dc94c49de0c7c5729eead513c
lunasa2307 <lunarisa.npr@gmail.com>
2026-02-24 20:35:38 +0800

    bump version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "me.kaylunasa.tahanapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "alpha-v0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
