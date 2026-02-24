7712094cf93dc525e2a09fe6f636e35f5a568170
lunasa2307 <lunarisa.npr@gmail.com>
2026-02-24 20:35:25 +0800

    update profile card

--- app/src/main/java/me/kaylunasa/tahanapp/adapter/ChildProfileCardAdapter.java
+++ app/src/main/java/me/kaylunasa/tahanapp/adapter/ChildProfileCardAdapter.java
@@ -1,5 +1,6 @@
 package me.kaylunasa.tahanapp.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,10 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.imageview.ShapeableImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import me.kaylunasa.tahanapp.R;
 import me.kaylunasa.tahanapp.activity.ChildViewActivity;
+import me.kaylunasa.tahanapp.data.Assessment;
 import me.kaylunasa.tahanapp.data.ChildProfile;
 
 public class ChildProfileCardAdapter extends RecyclerView.Adapter<ChildProfileCardAdapter.ChildProfileViewHolder> {
@@ -47,6 +51,20 @@ public class ChildProfileCardAdapter extends RecyclerView.Adapter<ChildProfileCa
                 holder.itemView.getContext().getResources().getText(R.string.years_old).toString(),
                 childProfile.getAge()
         ));
+        Date lastAssessmentDate = null;
+        for (Assessment assessment : childProfile.getAssessmentHistory()) if (lastAssessmentDate == null || lastAssessmentDate.getTime() < assessment.getTimestamp().getTime())
+            lastAssessmentDate = assessment.getTimestamp();
+        String lastAssessmentStr = holder.itemView.getContext().getResources().getText(R.string.never_assessed).toString();
+        if (lastAssessmentDate != null) {
+            @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("MMMM d, yyyy - hh:mm a");
+            lastAssessmentStr = sdf.format(lastAssessmentDate);
+        }
+
+
+        holder.lastAssessmentDisplay.setText(String.format(
+                holder.itemView.getContext().getResources().getText(R.string.last_assessment_format).toString(),
+                lastAssessmentStr
+        ));
         holder.view.setOnClickListener((v) -> {
             Log.d(TAG, "onBindViewHolder: attempting to access child '" + childProfile.getName() + "' at index " + pos + " for current user");
             Intent intent = new Intent(v.getContext(), ChildViewActivity.class);
@@ -68,6 +86,7 @@ public class ChildProfileCardAdapter extends RecyclerView.Adapter<ChildProfileCa
         ShapeableImageView imageDisplay;
         TextView nameDisplay;
         TextView ageDisplay;
+        TextView lastAssessmentDisplay;
 
         public ChildProfileViewHolder(@NonNull View view) {
             super(view);
@@ -76,6 +95,7 @@ public class ChildProfileCardAdapter extends RecyclerView.Adapter<ChildProfileCa
             this.imageDisplay = view.findViewById(R.id.imageView);
             this.nameDisplay = view.findViewById(R.id.childName);
             this.ageDisplay = view.findViewById(R.id.childAge);
+            this.lastAssessmentDisplay = view.findViewById(R.id.lastAssessment);
         }
     }
 }
